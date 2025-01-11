b1a0083502a20d3d52c6f25450419de706f86b05
Michael Kinnas <michaelkinnas@gmail.com>
2025-01-11 10:23:29 +0200

    restructured project

--- distill_test.py
+++ distill_test.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,7 +6,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import Subset
 from torch import hub
-from distillers import ResponseDistiller
+from trainers.distillers import ResponseDistiller
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -14,8 +15,8 @@ transforms_cifar = (transforms.Compose([
     transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]),
 ]))
 
-train_dataset = datasets.CIFAR10(root = './data', train = True, download = True, transform = transforms_cifar)
-test_dataset = datasets.CIFAR10(root = './data', train = False, download = True, transform = transforms_cifar)
+train_dataset = datasets.CIFAR10(root = f'{os.environ['DATASETS']}/CIFAR10/', train = True, download = True, transform = transforms_cifar)
+test_dataset = datasets.CIFAR10(root = f'{os.environ['DATASETS']}/CIFAR10/', train = False, download = True, transform = transforms_cifar)
 
 num_images = 1000
 training_subset = Subset(train_dataset, range(min(num_images, 50_000)))
@@ -35,3 +36,4 @@ optimizer = optim.Adam(student.parameters(), lr = 0.001)
 distiller = ResponseDistiller(teacher, student, optimizer, loss_fn, train_loader, test_loader, epochs=2, alpha=0.2, soft_loss_temp=5, final_loss_temp=1, seed=1000)
 distiller.distill(record_train_progress=False, use_tqdm=False)
 print(distiller.get_train_progress())
+
